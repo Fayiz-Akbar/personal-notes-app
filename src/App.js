2d50e5873aadc93da8a22ea0dd1e2e0fd0fdf590
Fayiz-Akbar <fayizakbar26@gmail.com>
2026-04-07 12:10:32 +0700

    file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './sstyles/App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import HomePage from './pages/HomePage';
+import ArchivePage from './pages/ArchivePage';
+import AddPage from './pages/AddPage';
+import DetailPage from './pages/DetailPage';
+import NotFoundPage from './pages/NotFoundPage';
+import './styles/App.css'; // Pastikan path CSS ini benar
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app-container">
+        <Navigation />
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/archives" element={<ArchivePage />} />
+            <Route path="/notes/new" element={<AddPage />} />
+            <Route path="/notes/:id" element={<DetailPage />} />
+            <Route path="*" element={<NotFoundPage />} /> {/* Menangkap URL 404 */}
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
