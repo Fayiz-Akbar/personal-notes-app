070b918c03d16bfd7ffc054030492e9319187544
Fayiz-Akbar <fayizakbar26@gmail.com>
2026-04-07 12:24:02 +0700

    finalisasi

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,32 @@
 import React from 'react';
+import { useSearchParams } from 'react-router-dom';
 import NoteList from '../components/NoteList';
+import SearchBar from '../components/SearchBar';
 import { getActiveNotes, deleteNote, archiveNote } from '../utils/local-data';
+
+function HomePageWrapper() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const keyword = searchParams.get('title') || '';
+
+  function changeSearchParams(keyword) {
+    setSearchParams({ title: keyword });
+  }
+
+  return <HomePage defaultKeyword={keyword} keywordChange={changeSearchParams} />;
+}
 
 class HomePage extends React.Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       notes: getActiveNotes(),
+      keyword: props.defaultKeyword || '',
     };
 
     this.onDeleteHandler = this.onDeleteHandler.bind(this);
     this.onArchiveHandler = this.onArchiveHandler.bind(this);
+    this.onKeywordChangeHandler = this.onKeywordChangeHandler.bind(this);
   }
 
   onDeleteHandler(id) {
@@ -20,16 +35,27 @@ class HomePage extends React.Component {
   }
 
   onArchiveHandler(id) {
-    archiveNote(id); // Memanggil fungsi bawaan Dicoding
+    archiveNote(id);
     this.setState({ notes: getActiveNotes() });
   }
 
+  onKeywordChangeHandler(keyword) {
+    this.setState({ keyword });
+    this.props.keywordChange(keyword);
+  }
+
   render() {
+    // Filter catatan berdasarkan keyword
+    const filteredNotes = this.state.notes.filter((note) => {
+      return note.title.toLowerCase().includes(this.state.keyword.toLowerCase());
+    });
+
     return (
       <section className="homepage">
         <h2>Catatan Aktif</h2>
+        <SearchBar keyword={this.state.keyword} keywordChange={this.onKeywordChangeHandler} />
         <NoteList 
-          notes={this.state.notes} 
+          notes={filteredNotes} 
           onDelete={this.onDeleteHandler} 
           onArchive={this.onArchiveHandler} 
         />
@@ -38,4 +64,4 @@ class HomePage extends React.Component {
   }
 }
 
-export default HomePage;
+export default HomePageWrapper;
---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NotFoundPage() {
+  return (
+    <section className="not-found">
+      <h2>404</h2>
+      <p>Halaman tidak ditemukan. Sepertinya kamu tersesat!</p>
+    </section>
+  );
+}
+
+export default NotFoundPage;
